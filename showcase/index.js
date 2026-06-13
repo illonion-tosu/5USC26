c17e7616be8c69ff013189118383328a77f06510
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2026-06-13 11:33:34 +1000

    fix: linting

--- showcase/index.js
+++ showcase/index.js
@@ -2,10 +2,8 @@ import { createTosuWsSocket } from "../_shared/core/websocket.js"
 
 // Beatmap information
 const roundNameEl = document.getElementById("round-name")
-let allBeatmaps
 async function getBeatmaps() {
     const response = await axios.get("../_data/showcase-beatmaps.json")
-    allBeatmaps = response.data.beatmaps
     roundNameEl.textContent = response.data.roundName
 }
 getBeatmaps()
