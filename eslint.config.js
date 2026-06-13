ab2b98ffb9f46dfef6983d4a71282e085160f900
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2026-06-13 11:32:46 +1000

    fix: misspelling

--- eslint.config.js
+++ eslint.config.js
@@ -30,7 +30,7 @@ module.exports = [
                 requestAnimationFrame: "readonly",
                 axios: "readonly",
                 CountUp: "readonly",
-                ComfyJS: "readyonly"
+                ComfyJS: "readonly"
             },
         },
         rules: {
