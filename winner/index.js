da7328ca34a01d471ad7bb1ab70d3d15ad641e26
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2026-06-15 19:24:39 +1000

    feat: complete winner scene

--- winner/index.js
+++ winner/index.js
@@ -1,11 +1,21 @@
+import { getCookie } from "../_shared/core/utils.js"
+
+// Match Winner Round
+const matchWinnerRoundEl = document.getElementById("match-winner-round")
+async function getBeatmaps() {
+    const response = await axios.get("../_data/beatmaps.json")
+    matchWinnerRoundEl.textContent = response.data.roundName
+}
+getBeatmaps()
+
 let allTeams
 async function getTeams() {
-    const response = await axios.get("../_data/beatmaps.json")
-    allTeams = response.data.beatmaps
+    const response = await axios.get("../_data/teams.json")
+    allTeams = response.data
 }
 getTeams()
 // Find Teams
-const findTeams = teamName => allBeatmaps.find(team => team.team_name === teamName)
+const findTeams = teamName => allTeams.find(team => team.team_name === teamName)
 
 let currentLeftTeamName, previousLeftTeamName, currentRightTeamName, previousRightTeamName
 let currentStarLeft, previousStarLeft, currentStarRight, previousStarRight
@@ -14,16 +24,19 @@ const teamNameEl = document.getElementById("team-name")
 const playerContainerEl = document.getElementById("player-container")
 
 setInterval(() => {
+    // Set information
     currentLeftTeamName = getCookie("currentLeftTeamName")
     currentRightTeamName = getCookie("currentRightTeamName")
     currentStarLeft = getCookie("currentStarLeft")
     currentStarRight = getCookie("currentStarRight")
 
+    // Compare information
     if (previousLeftTeamName !== currentLeftTeamName ||
         previousRightTeamName !== currentRightTeamName ||
         previousStarLeft !== currentStarLeft ||
         previousStarRight !== currentStarRight
     ) {
+        // Reset information
         previousLeftTeamName = currentLeftTeamName
         previousRightTeamName = currentRightTeamName
         previousStarLeft = currentStarLeft
@@ -39,9 +52,10 @@ setInterval(() => {
 
         // Get team
         const currentTeam = findTeams(winningTeam)
-        teamNameEl.textContent = currentTeam
+        teamNameEl.textContent = currentTeam.team_name
         playerContainerEl.innerHTML = ""
 
+        // Set players
         for (let i = 0; i < currentTeam.player_names.length; i++) {
             const player = document.createElement("div")
             player.textContent = currentTeam.player_names[i]
